56e32a098847dd3cd5fc974c70e63dfd2ada5225
Juan esteban <esteban66628@gmail.com>
2023-09-07 17:34:01 +0200

    services

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+// export const getAllTweetsService = async () => {
+//   const response = await fetch(`${process.env.REACT_APP_BACKEND}`);
+
+//   const json = await response.json();
+
+//   if (!response.ok) {
+//     throw new Error(json.message);
+//   }
+
+//   return json.data;
+// };

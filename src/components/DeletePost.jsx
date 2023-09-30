93c63457348f79748b47a5fe8f2bf4df151e067b
Juan esteban <esteban66628@gmail.com>
2023-09-30 13:28:06 +0200

    delete post css

--- src/components/DeletePost.jsx
+++ src/components/DeletePost.jsx
@@ -1,11 +1,13 @@
 import { useContext, useState } from 'react';
 import { deletePhotosService } from '../services';
 import { AuthContext } from '../context/AuthContext';
+import { useParams } from 'react-router-dom';
 
 function DeletePost({ photo, removePost }) {
   const { token, idUser, role } = useContext(AuthContext);
   const [error, setError] = useState('');
   const [deletePhoto, setDeletePhoto] = useState(false);
+  const actualUser = useParams().idUser;
 
   const openDeletePhoto = () => {
     setDeletePhoto(true);
@@ -29,16 +31,26 @@ function DeletePost({ photo, removePost }) {
 
   return (
     <>
-      {(role === 'admin' || (token && parseInt(photo.idUser) === parseInt(idUser))) && (
-        <button className='delete-post-button' onClick={openDeletePhoto}>
-          <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' className='bi bi-three-dots-vertical' viewBox='0 0 16 16'>
-            <path d='M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z' />
-          </svg>
-        </button>
-      )}
+      {role === 'admin' ||
+        (token &&
+          parseInt(photo.idUser) === parseInt(idUser) &&
+          (!actualUser ? (
+            <button className='delete-post-button-home-page' onClick={openDeletePhoto}>
+              <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' className='bi bi-three-dots' viewBox='0 0 16 16'>
+                <path d='M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z' />
+              </svg>
+            </button>
+          ) : (
+            <button className='delete-post-button-user-profile' onClick={openDeletePhoto}>
+              <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' className='bi bi-three-dots-vertical' viewBox='0 0 16 16'>
+                <path d='M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z' />
+              </svg>
+            </button>
+          )))}
+
       {deletePhoto && (
         <section className='modal-overlay'>
-          <div className='modal-content'>
+          <div className='modal-content delete-post-content'>
             <h2>Esto eliminará la publicación</h2>
             <p>¿Deseas continuar?</p>
             <div className='buttons-delete-post'>
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProviderComponent } from './context/AuthContext.jsx';
-import { ThemeProvider } from "./context/ThemeContext";
-import './index.css';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <ThemeProvider>
-      <AuthProviderComponent>
-        <App />
-      </AuthProviderComponent>
-    </ThemeProvider>
+      <ThemeProvider>
+        <AuthProviderComponent>
+          <App />
+        </AuthProviderComponent>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
